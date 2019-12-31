18d2ac3b159ce1fda845857da64ea9d56002b4d6
shouko oka <iamshk0522@gmail.com>
2019-12-31 18:42:24 +0900

    create message db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_31_091128) do
+ActiveRecord::Schema.define(version: 2019_12_31_092100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 2019_12_31_091128) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "comment"
+    t.bigint "chat_room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
+  end
+
+  add_foreign_key "messages", "chat_rooms"
 end
