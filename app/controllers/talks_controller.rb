d4d63e164bb735bd49efea95ed8171ea27ae6869
shouko oka <iamshk0522@gmail.com>
2020-01-03 15:53:46 +0900

    create controller and router for chatroom

---
+++ app/controllers/talks_controller.rb
@@ -0,0 +1,25 @@
+class TalksController < ApplicationController
+  def list
+    @chatroom = ChatRoom.all
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+    @message.update(message_params)
+    # {}
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to messages_path
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :details)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "talks", to: "talks#list"
+
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+
+  # get "tasks/:id", to: "tasks#show", as: :task
+
+  # get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  # post "tasks", to: "tasks#edit"
+
+  # patch "tasks/:id", to: "tasks#update"
 end
